c7b0ea1ad20952b4600df1549d3422bf7cc52e9b
ivan <ivanbilag@gmail.com>
2018-02-17 21:12:08 +0200

    add event to markers

--- Custom maps with google maps by david east/Mapster.js
+++ Custom maps with google maps by david east/Mapster.js
@@ -6,7 +6,41 @@
         }
 
         Mapster.prototype = {
+            zoom: function (level) {
+                if(level){
+                    this.googleMaps.setZoom(level);
+                }
+                else{
+                    return this.googleMaps;
+                }
+            },
+            _on: function (markerOptions) {
+                var self = this;
+                google.maps.event.addListener(markerOptions.object,markerOptions.event,function (e) {
+                    markerOptions.callback.call(self, e);
+                });
+            },
+            _createMarker: function (markerOptions) {
+              markerOptions.map = this.googleMaps;
+              return new google.maps.Marker(markerOptions);
+            },
+            addMarker: function (markerOptions) {
+                var marker;
+                markerOptions.position = {
+                    lat: markerOptions.lat,
+                    lng: markerOptions.lng
+                };
+                marker = this._createMarker(markerOptions);
 
+                if(markerOptions.event){
+                    this._on({
+                        object: marker,
+                        event: markerOptions.event.name,
+                        callback: markerOptions.event.callback
+                    });
+                }
+
+            }
         };
 
 
--- Custom maps with google maps by david east/script.js
+++ Custom maps with google maps by david east/script.js
@@ -1,13 +1,24 @@
-(function(window, google, mapster) {
+(function(window, mapster) {
     'use strict';
-
+    // aula 9
 
     var options = mapster.MAP_OPTIONS,
         element = document.getElementById('map'),
         map = mapster.create(element, options);
 
+    map.addMarker({
+        lat: 37.791350,
+        lng: -122.435883,
+        visible: true,
+        id: 18,
+        event: {
+            name: 'click',
+            callback: function () {
+                console.log('i like');
+            }
+        }
+    });
 
 
 
-
-}(window, google, window.Mapster || (window.Mapster = {})));
+}(window, window.Mapster));
